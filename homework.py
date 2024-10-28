95417ffd9de2cba7a0b88064cd8519b80c41dbca
quramboyev17 <quramboyev17@gmail.com>
2024-10-28 07:05:01 +0500

    Create new mini project

---
+++ homework.py
@@ -0,0 +1,64 @@
+# first hometask
+# def calculate_finance(monthly_income: float, tax_rate: float, rent: float, food: float, gym: float) -> None:
+#     yearly_salary: float = monthly_income * 12
+#     monthly_tax: float = monthly_income * (tax_rate / 100)
+#     monthly_expenses: float = rent + food + gym + monthly_tax
+#     monthly_net_income: float = monthly_income - monthly_expenses
+# 
+#     print('---------------------------')
+#     print(f'Ежемесячный доход: {monthly_income}$')
+#     print(f'Налог за месяц: {monthly_tax}$')
+#     print(f'Ежемесячные расходы: {monthly_expenses}$')
+#     print(f'Чистый доход (месяц): {monthly_net_income}$')
+#     print('---------------------------')
+# 
+# 
+# def main() -> None:
+#     monthly_income: float = float(input("Введите свой ежемесячный доход $: "))
+#     tax_rate: float = float(input('Введите ставку налога (%): '))
+#     rent: float = float(input('Введите сумму аренды: '))
+#     food: float = float(input('Введите ежемесячные расходы на еду: '))
+#     gym: float = float(input('Введите стоимость абонемента в спортзал: '))
+# 
+#     calculate_finance(monthly_income, tax_rate, rent, food, gym)
+# 
+# 
+# if __name__ == '__main__':
+#     main()
+
+def calculate_finance(monthly_income: float, tax_rate: float, currency: str) -> None:
+    yearly_salary: float = monthly_income * 12
+    monthly_tax: float = monthly_income * (tax_rate / 100)
+    yearly_tax: float = monthly_tax * 12
+    monthly_net_income: float = monthly_income - monthly_tax
+    yearly_net_income: float = yearly_salary - yearly_tax
+    print('---------------------------')
+    print(f'Ежемесячный доход: {currency}{monthly_income}$')
+    print(f'Налог за месяц: {currency}{monthly_tax}$')
+    print(f'Чистый доход за месяц: {currency}{monthly_net_income}$')
+    print(f'Годовой доход: {currency}{yearly_salary}$')
+    print(f'Уплаченный налог за год: {currency}{yearly_tax}$')
+    print(f'Чистый доход за год: {currency}{yearly_net_income}$')
+    print('---------------------------')
+
+
+def get_float_input(prompt: str) -> float:
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Ошибка! Введите число.")
+
+
+def main() -> None:
+    monthly_income: float = get_float_input('Введите свой ежемесячный доход $: ')
+    tax_rate: float = get_float_input('Введите свою ставку налога (%): ')
+
+    calculate_finance(monthly_income, tax_rate, currency='KR')
+
+
+if __name__ == '__main__':
+    main()
+
+x = open('homework.py', "w")
+x.close()
---
+++ main.py
@@ -0,0 +1,26 @@
+def calculate_finance(monthly_income: float, tax_rate: float, currency: str) -> None:
+    yearly_salary: float = monthly_income * 12
+    monthly_tax: float = monthly_income * (tax_rate / 100)
+    yearly_tax: float = monthly_tax * 12
+    monthly_net_income: float = monthly_income - monthly_tax
+    yearly_net_income: float = yearly_salary - yearly_tax
+    print('---------------------------')
+    print(f'Ежемесячный доход: {currency}{monthly_income}$')
+    print(f'Ставка налога: {currency}{monthly_tax}$')
+    print(f'Ежемесячный налог: {currency}{monthly_net_income}$')
+    print(f'Ежемесячный чистый доход: {currency}{yearly_salary}$')
+    print(f'Годовой уплаченный налог: {currency}{yearly_tax}$')
+    print(f'Годовой чистый доход: {currency}{yearly_net_income12}$')
+    print('---------------------------')
+calculate_finance(100.1234, 20, '$')
+
+def main() -> None:
+    monthly_income: float = float(input('Введите свой ежемесячный доход $: '))
+    tax_rate: float = float(input('Введите свой ставка налога (%): '))
+
+    calculate_finance(monthly_income, tax_rate, currency='KR')
+
+if __name__ == '__main__':
+    main()
+x = open('homework.py', "w")
+x.close()
